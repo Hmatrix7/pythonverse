3b562fd00df1a42a3905229900b8cb668035128b
Barrack.k <Barrackkainga@gmail.com>
2025-09-27 11:54:17 +0300

    First commit

---
+++ DataAnalyst101 Basics.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Hello Data Analyst")
+
+
+# In[2]:
+
+
+fruits=["Apples","Oranges","Bananas"]
+print (0)
+
+
+# In[3]:
+
+
+print(fruits)
+
+
+# In[4]:
+
+
+print(fruits [0])
+
+
+# In[5]:
+
+
+coordinates = (10.5, 20.3)
+print(coordinates[1])   # 20.3
+
+
+# In[6]:
+
+
+employee = {
+    "name": "Aisha",
+    "age": 30,
+    "department": "HR"
+}
+print(employee["name"])    # Aisha
+
+
+# In[9]:
+
+
+name="Barrack"
+age=21
+salary=500000
+data_analyst =True
+print(name,age,salary,data_analyst)
+
+
+# In[17]:
+
+
+list_food=["Meat","Tea","Rice"] # Square bracket  for list used for non-constant values
+data_constant=(2003,2025) # round bracket for tuple, used for constant values
+unique_numbers = {1, 2, 3, 3, 4} # Curly Bracket for set, used to clean data remove duplicates etc
+
+data_dictionary={
+    "name":"Barrack",
+    "age": 21,
+    "dream_job":"Data_Analyst"
+}
+print(data_dictionary["dream_job"])
+
+
+# In[ ]:
+
+
+
+
---
+++ DataAnalyst102 Operators and Expressions.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+a=2
+b=3
+c=5
+d=7
+e=9
+f=10
+g=2
+
+
+# In[30]:
+
+
+print(a*b) #multiplication
+
+
+# In[31]:
+
+
+print(a%b) #Modulus Operator (%): Returns the remainder after division
+print(f%g)
+
+
+# In[32]:
+
+
+print(2**3) #In Python, the ** operator is used for exponentiation—raising a number to the power of another.
+
+
+# In[33]:
+
+
+print(5 == 5)   # True    # Comparison Operators (always return True or False
+print(5 != 3)   # True    # uses symbols ==, !=, <, >, <=, >=
+print(5 > 10)   # False
+
+
+# In[48]:
+
+
+x = True        # logic operators
+y = False       # uses words and,or,not 
+print(x and y)  # False if both are true
+print(x or y)   # True if one is true
+print(not x)    # False reverses 
+
+
+# In[49]:
+
+
+num = 10   #Assignment Operators =, +=, -=, *=, /=, etc.
+num += 5   # same as num = num + 5
+print(num) # 15
+
+
+# In[50]:
+
+
+fruits = ["apple", "banana", "mango"]   #Membership Operators in, not in
+print("apple" in fruits)     # True
+print("orange" not in fruits) # True
+print("orange" in fruits)
+
+
+# In[ ]:
+
+
+
+
